8e72e2b0789985f1706a1eb174913fb46ce65fcb
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-04-01 10:56:53 -0500

    working on projects page now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,9 +7,9 @@ class Projects extends React.Component{
             <div className='flex-container'>   
                     <h2 id='main-header'>Mercy Health Appointment Center</h2>
                         <div className='image-container'>
-                                <img src={require('../images/mercy_home.png')} className='images'id='first-image'/>
-                                <img src={require('../images/mercy_book.png')} className='images'/>
-                                <img src={require('../images/mercy_details.png')} className='images'/>
+                                <img src={require('../images/mercy_main.svg')} className='images'id='first-image'/>
+                                <img src={require('../images/mercy_appointments.svg')} className='images'/>
+                                <img src={require('../images/mercy_details.svg')} className='images'/>
                         </div>
                         <div className='projects-paragraph'>
                             I decided to do Mercy Clinic Appointment Center when I was booking my appointment with my doctor whom does not have online booking. I have to call everytime, ask for availability, and the worst part is I have to call during open hours. Through an app like this one, I can book, cancel, or update my appointment at my own convenience. 
