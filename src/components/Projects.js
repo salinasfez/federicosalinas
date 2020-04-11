e4efaaf793d853cdb21760f8e862667df2c72bed
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-04-11 03:39:42 -0500

    did some changed to my projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Projects.css';
+import mercyThumbnail from '../images/mercy_main.svg'
 
 class Projects extends React.Component{
     render(){
     return(
             <div className='flex-container'>   
-                    <h2 id='main-header'>Mercy Health Appointment Center</h2>
+                    {/* <h2 id='main-header'>Mercy Health Appointment Center</h2> */}
                         <div className='image-container'>
-                                <img src={require('../images/mercy_main.svg')} className='images'id='first-image'/>
-                                <img src={require('../images/mercy_appointments.svg')} className='images'/>
-                                <img src={require('../images/mercy_details.svg')} className='images'/>
+                                <a target='_blank' href={mercyThumbnail}><img src={require('../images/mercy_main.svg')} className='images'id='first-image'/></a>
+                                {/* <img src={require('../images/mercy_appointments.svg')} className='images'/>
+                                <img src={require('../images/mercy_details.svg')} className='images'/> */}
+                                <div className='project-details'>
+                                     <h5> Mercy Clinic Appointment Center</h5><br></br>
+                                    <p> Technologies: HTML5, Bootstrap, React, Javascript, MongoDB, Express</p>
+                                    <a className='project-links' href={'https://mercy-appointment-center.herokuapp.com/appointments'} target='_blank'>Project link ---></a>
+                                </div>
                         </div>
-                        <div className='projects-paragraph'>
-                            I decided to do Mercy Clinic Appointment Center when I was booking my appointment with my doctor whom does not have online booking. I have to call everytime, ask for availability, and the worst part is I have to call during open hours. Through an app like this one, I can book, cancel, or update my appointment at my own convenience. 
-                            <br/><br/><a className='project-links' href={'https://mercy-appointment-center.herokuapp.com/appointments'} target='_blank'>Project link...</a>
-                        </div>
-                    </div>
+            </div>
         )
     }
 }
