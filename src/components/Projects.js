d4fda934d61ab769560a37137d7d9be0db60dab8
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-04-18 12:55:03 -0500

    added my budget app project to the portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,19 +15,18 @@ class Projects extends React.Component{
             <div className='flex-container'>   
                     {/* <h2 id='main-header'>Mercy Health Appointment Center</h2> */}
                         <div className='image-container'>
-                                <a target='_blank' href={'https://merchant-clothing-co.herokuapp.com/'}><img src={require('../images/storesvg.svg')} className='images'id='first-image'/></a>
+                                <a target='_blank' href={'https://federico-budget-app.herokuapp.com/'}><img src={require('../images/budget-app.svg')} className='images'id='first-image'/></a>
                                 {/* <img src={require('../images/mercy_appointments.svg')} className='images'/>
                                 <img src={require('../images/mercy_details.svg')} className='images'/> */}
                                 <div className='project-details'>
-                                     <h5> Merchant Clothing Co.</h5><br></br>
-                                    <p> Technologies used: HTML5, Bootstrap, React, Javascript, Ruby on Rails</p>
-                                    <p>Description: Upload product images, delete items, and list an item.</p>
-                                    <a className='project-links' href={'https://merchant-clothing-co.herokuapp.com/'} target='_blank'>Project link ---></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a target='_blank' href='https://github.com/salinasfez/online_store'>Github Back End Code ---></a>&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a target='_blank' href='https://github.com/salinasfez/online_store_frontend'>Github Front End Code ---></a>
+                                     <h5> Budget Application</h5><br></br>
+                                    <p> Technologies used: HTML5, Bootstrap, React, Javascript, Express, MongoDB</p>
+                                    <p>Description: Keep track of your money by using this budgeting app.</p>
+                                    <a className='project-links' href={'https://federico-budget-app.herokuapp.com/'} target='_blank'>Project link ---></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a target='_blank' href='https://github.com/salinasfez/budget-app'>Github Code ---></a>
                                 </div>
                         </div>
-                        <div className='image-container'>
+                        <div className='image-container second-project'>
                                 <a target='_blank' href={'https://mercy-appointment-center.herokuapp.com/appointments'}><img src={require('../images/mercy_main.svg')} className='images'id='first-image'/></a>
                                 {/* <img src={require('../images/mercy_appointments.svg')} className='images'/>
                                 <img src={require('../images/mercy_details.svg')} className='images'/> */}
