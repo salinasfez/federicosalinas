b6bab89c7b547850d7d6726b9727a7236734e42b
Federico Salinas <federicosalinas@Federicos-MacBook-Air.local>
2020-09-28 15:56:09 -0500

    working on icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,12 +13,12 @@ class Projects extends React.Component{
     render(){
     return(
             <div className={classes.Projects}>   
-                    {/* <h2 id='main-header'>Mercy Health Appointment Center</h2> */}
-                        <div className='image-container'>
-                                <a target='_blank' href={'https://federico-budget-app.herokuapp.com/'}><img src={require('../images/budget-app.svg')} className='images'id='first-image'/></a>
+                        <h3 className={classes.imageContainer}>Projects</h3>
+                        <div className={classes.imageContainer}>
+                                <a target='_blank' href={'https://federico-budget-app.herokuapp.com/'}><img src={require('../images/budgetapp.png')} className={classes.images} id='first-image'/></a>
                                 {/* <img src={require('../images/mercy_appointments.svg')} className='images'/>
                                 <img src={require('../images/mercy_details.svg')} className='images'/> */}
-                                <div className='project-details'>
+                                <div className={classes.projectDetails}>
                                      <h5> Budget Application</h5><br></br>
                                     <p> Technologies used: HTML5, Bootstrap, React, Javascript, Express, MongoDB</p>
                                     <p>Description: Keep track of your money by using this budgeting app.</p>
@@ -26,23 +26,23 @@ class Projects extends React.Component{
                                     <a target='_blank' href='https://github.com/salinasfez/budget-app'>Github Code ---></a>
                                 </div>
                         </div>
-                    <div className='image-container second-project'>
-                                <a target='_blank' href={'http://eightballinktattoo.com/'}><img src={require('../images/8ink.svg')} className='images'id='first-image'/></a>
+                    <div className={classes.imageContainer}>
+                                <a target='_blank' href={'http://eightballinktattoo.com/'}><img src={require('../images/eightball.png')} className={classes.images} id='first-image'/></a>
                                 {/* <img src={require('../images/mercy_appointments.svg')} className='images'/>
                                 <img src={require('../images/mercy_details.svg')} className='images'/> */}
-                                <div className='project-details'>
+                                <div className={classes.projectDetails}>
                                      <h5> 8ball Ink Tattoo</h5><br></br>
                                     <p> Technologies used: HTML5, Bootstrap, React, Javascript, Express</p>
                                     <p>Description: Business app able to send messages directly to owners mobile.</p>
-                                    <a className='project-links' href={'http://eightballinktattoo.com/'} target='_blank'>Project link ---></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <a className='project-links' href={'http://eightballinktattoo.com/'} target='_blank'>Website link ---></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                     <a target='_blank' href='https://github.com/salinasfez/eightballinktatoo'>Github Code ---></a>
                                 </div>
                         </div>
-                        <div className='image-container second-project'>
-                                <a target='_blank' href={'https://mercy-appointment-center.herokuapp.com/appointments'}><img src={require('../images/mercy_main.svg')} className='images'id='first-image'/></a>
+                        <div className={classes.imageContainer}>
+                                <a target='_blank' href={'https://mercy-appointment-center.herokuapp.com/appointments'}><img src={require('../images/mercyclinic.png')} className={classes.images} id='first-image'/></a>
                                 {/* <img src={require('../images/mercy_appointments.svg')} className='images'/>
                                 <img src={require('../images/mercy_details.svg')} className='images'/> */}
-                                <div className='project-details'>
+                                <div className={classes.projectDetails}>
                                      <h5> Mercy Clinic Appointment Center</h5><br></br>
                                     <p> Technologies used: HTML5, Bootstrap, React, Javascript, MongoDB, Express</p>
                                     <p>Description: Create, edit, and delete appointments.</p>
